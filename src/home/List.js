1b0afa267894e22576928c4aedbebb395bac7113
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-12-01 20:10:31 -0200

    extract components and extract css

---
+++ src/home/List.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import CustomCard from '../common/CustomCard'
+import { faShoppingBasket, faCheck } from '@fortawesome/free-solid-svg-icons'
+import './list.css'
+import ListFooter from './ListFooter'
+import ListItem from './ListIem'
+
+
+const List = () => (
+  <CustomCard
+    containerClass='list-container'
+    footer={<ListFooter />}
+  >
+    <div>
+      <p className='title'>Mês</p>
+      <div className='list-card-body'>
+        <ListItem icon={ faShoppingBasket } text='1 Item restante'/>
+        <ListItem icon={ faCheck } text='2 Item comprados'/>
+      </div>
+    </div>
+  </CustomCard>
+)
+
+export default List
