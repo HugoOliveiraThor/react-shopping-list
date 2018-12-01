d58293f29535936a2ec826afe7ca71b37d14109d
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-12-01 11:28:03 -0200

    Add theme from material ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { createMuiTheme } from '@material-ui/core/styles'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#00bcd4'
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      Ola
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          Ola
       </div>
+      </MuiThemeProvider>
     )
   }
 }
