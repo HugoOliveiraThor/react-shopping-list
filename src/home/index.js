9b4a6f700610f58da7fcd6fe2065dfda761b130e
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-12-01 19:27:34 -0200

    Creating footer and list shopping

--- src/home/index.js
+++ src/home/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardContent from '@material-ui/core/CardContent'
+import { Card, CardActionArea, CardContent, CardActions, Divider } from '@material-ui/core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingBasket, faCheck } from '@fortawesome/free-solid-svg-icons'
 import './home.css'
+
 
 const Home = () => (
   <div className='page-container'>
@@ -15,6 +16,33 @@ const Home = () => (
             </div>
           </CardContent>
         </CardActionArea>
+      </Card>
+    </div>
+
+    <div className='list-container'>
+      <Card className='card'>
+        <CardActionArea className='card-action-area'>
+          <CardContent className='card-content'>
+            <div>
+              <p className='title'>Mês</p>
+              <div className='list-card-body'>
+                <div className='list-card-item'>
+                  <FontAwesomeIcon icon={faShoppingBasket} size='sm' />
+                  <p>1 Item(s) restante</p>
+                </div>
+                <div className='list-card-item'>
+                  <FontAwesomeIcon icon={faCheck} size='sm'/>
+                  <p>2 Item(s) comprados</p>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </CardActionArea>
+        <Divider />
+        <CardActions className='card-footer'>
+          <p>01/01/2018</p>
+          <p>100.00</p>
+        </CardActions>
       </Card>
     </div>
   </div>
