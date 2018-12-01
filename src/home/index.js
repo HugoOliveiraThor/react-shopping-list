e52c4b7e065732d702ca1f3c02bee6cceb3d06d7
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-12-01 19:07:38 -0200

    Adding the css of home shopping list

--- src/home/index.js
+++ src/home/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardContent from '@material-ui/core/CardContent'
+import './home.css'
 
 const Home = () => (
-  <div>
-    <h1>Home Page</h1>
+  <div className='page-container'>
+    <div className='new-list-container'>
+      <Card className='card'>
+        <CardActionArea className='card-action-area'>
+          <CardContent className='card-content'>
+            <div>
+              <p className='title'>Adicionar novas listas</p>
+            </div>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
   </div>
 )
 
