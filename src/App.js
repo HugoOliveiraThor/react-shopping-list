c40ea7f5b1b39b8eb343842e15ceab524037a652
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-12-01 11:38:23 -0200

    Add roboto family fonts Prepare the new css files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { createMuiTheme } from '@material-ui/core/styles'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import AppBar from '@material-ui/core/AppBar'
 import './App.css';
 
 const theme = createMuiTheme({
@@ -18,9 +19,11 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <div className="App">
-          Ola
-      </div>
+        <AppBar position='static' color='primary'>
+        <div className='header'> 
+          <p className='title'>Lista de compras</p>
+        </div>
+      </AppBar>
       </MuiThemeProvider>
     )
   }
