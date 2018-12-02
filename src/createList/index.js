ed4419316712753f6d0cb883b192c1125efe4b75
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-12-02 18:52:59 -0200

    Creating the card of the product

--- src/createList/index.js
+++ src/createList/index.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Checkbox , Typography } from '@material-ui/core'
 import './list.css'
 import Form from './Form'
+import CustomCard from '../common/CustomCard'
 
 const CreateList = () => (
   <div className='page-container'>
     <Form />
+    <CustomCard 
+      footer='' 
+      containerClass='list-item' 
+      link='#'>
+      <div>
+        <div className='list-item-header'>
+          <Typography variant='subtitle1' component='h2'>Café</Typography>
+          <Checkbox />
+        </div>
+          <Typography component='p'>1 Unidade</Typography>
+          <Typography component='p'>R$ 10.00</Typography>
+      </div>
+    </CustomCard>
+    <div className='list-items-container'>
+
+    </div>
+
   </div>
 )
 
